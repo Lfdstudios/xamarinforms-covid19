c9029379fdd501a909e2618190485a36d283de2e
Lfdstudios <54958946+Lfdstudios@users.noreply.github.com>
2020-10-06 15:08:12 -0400

    Initial commit

---
+++ xamarinforms-covid19-master-master/xamarinforms-covid19-master/XFCovid19/XFCovid19/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Plugin.Multilingual;
+using System.Globalization;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using XFCovid19.Resources;
+
+namespace XFCovid19
+{
+    public partial class App : Application
+    {
+        public static string AppCultureInfo;
+
+        public App()
+        {
+            InitializeComponent();
+
+            AppCultureInfo = Preferences.Get("appLanguage", "pt");
+            AppResources.Culture = new CultureInfo(AppCultureInfo);
+            CrossMultilingual.Current.CurrentCultureInfo = new CultureInfo(AppCultureInfo);
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
